fe8b473de3b5d933106bca9d0a3bdda79e5531cd
ChuckShuleyq <105976166+ChuckShuleyq@users.noreply.github.com>
2023-02-26 15:47:07 +0000

    new file:   .vscode/settings.json 	new file:   bot.cpp 	modified:   main.py 	new file:   tgbot-cpp

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def f(x, t, k):
-    if x == t: return k == 15
-    if x > t: return 0
-    if x < t: return f(x + 2, t, k+1) + f(x + 5, t, k+1) + f(x + 5, t, k+1)
-for a in range(2, 10000):
-    if f(1,a,0)==1:
-        print(a)
+d = set()
+def f(c, s):
+    if s == 15:
+        d.add(c)
+    
+    else:
+        f(c * 2, s +1)
+        f(c *2 + 1, s +1)
+f(1, 0)
+print(len(d))
